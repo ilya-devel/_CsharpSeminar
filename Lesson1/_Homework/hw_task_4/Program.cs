a5a9f89b77a2003aa298a750d825f16c83ef3ba1
Ilya.devel <ilya.devel@yandex.ru>
2022-08-19 00:42:47 +0500

    Some fix in homework

--- Lesson1/_Homework/hw_task_4/Program.cs
+++ Lesson1/_Homework/hw_task_4/Program.cs
@@ -8,13 +8,14 @@ namespace hwFour
         {
             Console.WriteLine("Enter positive integer number: ");
             int num = Convert.ToInt32(Console.ReadLine());
+            if (num == 0) Console.WriteLine("You entered zero!");
             if (num < 0)
             {
                 Console.WriteLine("You entered a negative number, we fixed it");
                 num = num * -1;
             }
             int i = 2;
-            while (i < num)
+            while (i <= num)
             {
                 if (i+2 > num) Console.Write(i);
                 else Console.Write(i + ", ");
